715402ca7174d0913502638cc8dc8b55fa48846b
Iryna <iriskag@ukr.net>
2019-06-30 21:44:55 +0300

    Updates

---
+++ precache-manifest.9f1b6f3ede17b791dbe4abcabaef411f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6261fe16e29b4b9dbb6222115002ccb6",
+    "url": "/react_static-list-of-todos/index.html"
+  },
+  {
+    "revision": "76ac916174911bd4d8d4",
+    "url": "/react_static-list-of-todos/static/css/main.b7f1cd70.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/react_static-list-of-todos/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "76ac916174911bd4d8d4",
+    "url": "/react_static-list-of-todos/static/js/main.27afbd6f.chunk.js"
+  },
+  {
+    "revision": "ea6f31786c2aed313835",
+    "url": "/react_static-list-of-todos/static/js/runtime~main.00529491.js"
+  }
+]);
